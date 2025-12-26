7117f6627483315825af44a48ab39220ef5ca646
aravin-narayan <arnie.education@gmail.com>
2025-12-26 14:27:11 -0800

    Moving files to HelloWorld Directory where it should belong.

--- C++/HelloWorld/main.cpp
+++ C++/HelloWorld/main.cpp
@@ -2,7 +2,9 @@
 
 using namespace std;
 
+// This is a simple C++ program that prints "Hello, World!" to the console.
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
+
 }
